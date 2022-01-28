e9fde330c589b413900f0e203cff4478a7574d41
JoanR17 <joanr.m.1999@gmail.com>
2022-01-28 10:51:40 +0100

    Update ejercicio6.sql

--- ejercicio6.sql
+++ ejercicio6.sql
@@ -23,10 +23,11 @@ DROP TABLE IF EXISTS `Peliculas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Peliculas` (
-  `Identity` int NOT NULL,
+  `Codigo` int NOT NULL,
   `Nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `CalificacionEdad` int DEFAULT NULL,
-  PRIMARY KEY (`Identity`)
+  PRIMARY KEY (`Codigo`),
+  UNIQUE KEY `Codigo_UNIQUE` (`Codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,12 +48,13 @@ DROP TABLE IF EXISTS `Salas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Salas` (
-  `Identity` int NOT NULL,
+  `Codigo` int NOT NULL,
   `Nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `Pelicula` int DEFAULT NULL,
-  PRIMARY KEY (`Identity`),
+  PRIMARY KEY (`Codigo`),
+  UNIQUE KEY `Codigo_UNIQUE` (`Codigo`),
   KEY `Pelicula_idx` (`Pelicula`),
-  CONSTRAINT `Pelicula` FOREIGN KEY (`Pelicula`) REFERENCES `Peliculas` (`Identity`)
+  CONSTRAINT `Pelicula` FOREIGN KEY (`Pelicula`) REFERENCES `Peliculas` (`Codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-28 10:17:47
+-- Dump completed on 2022-01-28 10:51:03
